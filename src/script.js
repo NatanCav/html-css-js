ded71c36942141c2eb484c86e4fbb68925392640
NatanCav <wylucasyw@gmail.com>
2026-02-04 22:50:12 -0300

    Reforçando conceito de variáveis

---
+++ src/script.js
@@ -0,0 +1,36 @@
+console.log("Digite um nome do seu jogador.");
+
+// declara uma variável
+let nickname = "NatanCav"
+
+// concatenando mensagem + variável
+console.log("Bem Vindo " + nickname)
+
+// Conteúdo fixo se torna uma constante.
+const notificacao = "Pokemon Go diz: "
+
+console.log(notificacao + "tem um novo pokemon na região!")
+
+// pokemon
+
+// string - são variaveis que armazenam texto
+// number - são variaveis que armazenam números
+// boolean - são variaveis que armazenam valores verdadeiros ou falsos (true ou false)
+
+let nomePokemon = "Pikachu"
+let pokemonSexo = "M"
+let nivelPokemon = 20
+let pontosVidaPokemon = 150
+let selecionavel = true
+
+// EXERCÍCIO 
+
+//nome, idade, número, CPF, endereço e recebe pensão?
+// String, Number, Boolean
+
+let nomePessoa = "Sônia"
+let idadePessoa = 59
+let numeroPessoa = "(81)98765-4321"
+let cpfPessoa = "123.456.789-00"
+let enderecoPessoa = "Rua das Flores, 123, Santa Cruz do Capibaribe - PE"
+let recebePensao = true
